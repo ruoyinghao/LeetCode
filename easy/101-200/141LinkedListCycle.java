26c270d027570ce9e71de896ac12cd07b7126228
Ruoying Hao <ruoyinghao@outlook.com>
2016-07-22 23:37:03 +0800

    Create 141LinkedListCycle.java

---
+++ easy/101-200/141LinkedListCycle.java
@@ -0,0 +1,26 @@
+//Given a linked list, determine if it has a cycle in it. 
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
